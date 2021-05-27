55115310b0732f8ac58a1bb8020c64a0252f0caf
Aaryan Arora <aaryan2134@gmail.com>
2021-05-27 15:29:43 +0530

    Update AgroBot_Autonomous_Motion.ino

--- AgroBot_Autonomous_Motion/AgroBot_Autonomous_Motion.ino
+++ AgroBot_Autonomous_Motion/AgroBot_Autonomous_Motion.ino
@@ -135,6 +135,6 @@ int ultraFront() {
   duration = pulseIn(ECHO_PIN1, HIGH);
   
   // Convert the time into a distance
-  cm = (duration / 2) / 29.1;   // Divide by 29.1 
+  cm = (duration / 2) / 0.034;   // Multiply by 0.034 
   return cm;
 }
